31a2a3d87128785328fa1eed08f0af9c19acfe91
Joshua Adams <N7down@gmail.com>
2013-11-23 16:42:57 -0500

    finished clustering

---
+++ Python/part2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from TagExtractor import ReuterRooter as RR
+import re
+
+'''
+Notes:
+http://scikit-learn.org/stable/modules/clustering.html
+http://stackoverflow.com/questions/18364026/clustering-values-by-their-proximity-in-python-machine-learning
+
+Use k-means to cluster?
+https://www.google.com/search?q=k+means+clusting+example+python&oq=k+means+&aqs=chrome.0.69i59j69i57j0l4.2577j0j4&sourceid=chrome&espv=210&es_sm=91&ie=UTF-8
+http://stackoverflow.com/questions/1545606/python-k-means-algorithm
+http://hackmap.blogspot.com/2007/09/k-means-clustering-in-scipy.html
+http://scikit-learn.org/stable/auto_examples/document_clustering.html
+
+1. use the feature vector to cluster
+2. cluster into 8 different clusters
+'''
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
