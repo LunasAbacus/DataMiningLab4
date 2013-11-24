0462d6476d8806c5691ce03512a037d8df56f330
LunasAbacus <lunasabacus@gmail.com>
2013-11-24 16:24:17 -0500

    16 Cluster initial feature extraction

--- Python/FeatureExtractor.py
+++ Python/FeatureExtractor.py
@@ -139,7 +139,7 @@ def main():
 
 			for title in titleSet:
 				wordNum = 0
-				wordClusterMap = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0}
+				wordClusterMap = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0}
 
 				#print ("articleNumber: " + str(title))
 				for word in topWords:
